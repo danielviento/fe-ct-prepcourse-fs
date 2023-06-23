d4129839e138ed40a07be3fa3582b744feb27bcf
danif <danielviento@gmail.com>
2023-06-22 22:21:02 -0300

    ej ok hasta num mas grande

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -112,38 +112,49 @@ function arrayContiene(array, elemento) {
 
 
 var suma=0;
-var Nums=[2,6,8,10];
+var nums=[2,6,8,10];
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
    var long=arrayOfNums.length;
-   console.log(long);
+   //console.log(long);
    for (let i=0; i<long; i++){// long lo habia puesto entre parentesis y no hacia falta
-    console.log(arrayOfNums[i]);// para acceder a un elementoo del array tengo que ponerlo entre corchetes
+    //console.log(arrayOfNums[i]);// para acceder a un elementoo del array tengo que ponerlo entre corchetes
         suma += arrayOfNums[i]; // la variable suma tenia que declararla antes del loop
-   };
+   }
    console.log(suma);
    return suma;
+}
+ agregarNumeros(nums);
 
-}; agregarNumeros(Nums);
-
-
+//===================================================================================
 //hasta aca ok 19/6 23:42
-
-
-
-
-
-
-
-
+var notas=[8, 7, 9, 10, 8];
+var sum=0;
+var cuantasNotas=0;
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+    cuantasNotas=resultadosTest.length;
+    console.log("cantidad de notas " + cuantasNotas);
+    for (let i=0; i<cuantasNotas; i++){
+      sum+=resultadosTest[i];
 }
+    console.log("esta es la suma " +sum);
+   console.log("promedio " + (sum/cuantasNotas));
+   var promedio=sum / cuantasNotas;
+   return promedio;
+}
+promedioResultadosTest(notas);
 
+
+
+
+
+// este ejercicio esta bien=================================================
+var grande=[10,9,25,6];
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
@@ -157,6 +168,9 @@ function multiplicarArgumentos() {
    // Tu código:
 }
 
+
+
+//=========================================================
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
